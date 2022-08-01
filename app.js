8fb1f88eded9cd0e8d683b690e4714ccc406a478
chrizzy-ghold <akubernicetay@gmail.com>
2022-08-01 16:05:30 +0000

    javascript

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+// DOM manipulation
+document.getElementById("first").innerHTML = "my first dom manipulation";
+
+//change the color of the text
+document.getElementById("first").style.color="yellow";
+
+//change the background color of the text
+document.getElementById("first").style.backgroundColor="blue";
+
+//change the font size of the text
+document.getElementById("first").style.fontSize = "70px"; 
+
+//display the text as none
+document.getElementById("first").style.display = "none";
